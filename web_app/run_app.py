c5eebb323cc004a846e1c5943f5cafad748e92b1
Haltoen <hjaltipp@hotmail.com>
2023-05-17 20:58:51 +0200

    forms added to website

--- web_app/run_app.py
+++ web_app/run_app.py
@@ -1,11 +1,49 @@
-from flask import Flask, render_template, url_for
-
+from datetime import datetime
+from flask import Flask, render_template, url_for, flash, redirect, g 
+from flask_sqlalchemy import SQLAlchemy
 import time
+import sqlite3
+from forms import DeployAgent_form , MakeTweet_form
+
 current_time = time.time()
 
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "very_secret_key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 
 
+DATABASE = '/path/to/database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+"""
+db=SQLAlchemy(app)
+class User(db.Model):
+    name = db.Column(db.String(20), primary_key=True)
+
+    def __repr__(self):
+            return f"User('{self.name}')"
+
+class Tweet(db.Model):
+    id = db.Column(db.Integer, Primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    content = db.Column(db.String(200), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Tweet('{self.name}','{self.content}','{self.date}')"
+"""
 
 #TOODO implemnet fetch feed
 latest_tweets = [
@@ -30,6 +68,22 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html", title = "about")
+
+@app.route("/deploy", methods=["GET", "POST"])
+def deploy():
+    form = DeployAgent_form()
+    if form.validate_on_submit():
+        flash(f'deployed agent: {form.name.data}!', "succes")
+        return redirect(url_for("home"))
+    return render_template("deploy.html", title="deploy agent", form = form)
+
+@app.route("/tweet", methods=["GET", "POST"])
+def tweet():
+    form = MakeTweet_form()
+    if form.validate_on_submit():
+        flash("tweet sent", "succes")
+        return redirect(url_for("home"))
+    return render_template("tweet.html", title="make tweet", form = form)
 
 if __name__ == "__main__":
     app.run(debug=True)
