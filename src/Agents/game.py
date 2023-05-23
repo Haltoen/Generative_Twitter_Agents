d6a066695a0edabad2aef90ff4f0e9df86b7ef01
Haltoen <hjaltipp@hotmail.com>
2023-05-23 15:25:13 +0200

    små changes

--- src/Agents/game.py
+++ src/Agents/game.py
@@ -1,17 +1,34 @@
 from typing import List, Tuple
 from agent import Agent
 from utils.functions import embed, create_embedding_bytes, profile
+import time
 class Twitter:
-    def __init__(self, agents: List[Tuple[str,str, bool]]) -> None:
-        self.agents = [Agent(name, description, 150, use_openai, 0) for name, description, use_openai in agents]
-        
-    def run(self):
-        for _ in range(10):
-            for agent in self.agents:
-                feed = agent.recommend_feed()
-                agent.view_feed(feed)
+    def __init__(self) -> None:
+        self.agents = []
+        self.current_agent_index = 0    
 
-game = Twitter([("DOnald Trump", "you are the former president of the us, you want to build a wall and hate nancy polosi, every tweet you make is a rhyme", True),
+
+    def run(self):
+        while not self.paused:
+            current_agent = self.agents[self.current_agent_index]
+            feed = current_agent.recommend_feed()
+            current_agent.view_feed(feed)
+            self.current_agent_index = (self.current_agent_index + 1) % len(self.agents)
+        time.sleep(1)  # Wait for 1 second before the next iteration
+
+    def pause(self):
+        self.paused = True
+
+    def unpause(self):
+        if self.agents:
+            self.paused = False
+            self.run()
+
+    def add_agent(agent:Agent):
+        self.agents += [agent]
+        
+
+game = Twitter([("DOnald Trump", "you are the former president of the us, you want to build a wall and hates nancy polosi, every tweet you make is a rhyme", True),
                ])
 
 game.run()
