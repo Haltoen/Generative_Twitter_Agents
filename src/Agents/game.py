e0ac16f31be7c016a029891108d99917509f908e
HP2706 <92665875+HP2706@users.noreply.github.com>
2023-06-06 13:43:08 +0200

    deleting tests

--- src/Agents/game.py
+++ src/Agents/game.py
@@ -48,11 +48,3 @@ class Agent_Manager:
     def add_agent(self, agent:Agent):
         self.agents.append(agent)
 
-from utils.functions import embed
-import numpy as np
-        
-agent = Agent("test", "test", 100, False)
-xq = embed(["Donald trump"])# this is the latency bottleneck
-xq = np.array([xq.embeddings[0]]) 
-out = agent._twitter_db.similarity_search(xq, 10, True)
-print(out)
